183c9fec4ac54ab8f2c9736d1fe9461544f90f14
Malte Hübner <maltehuebner@gmx.org>
2022-10-21 23:25:54 +0200

    More bundle stuff.

--- src/MaltehuebnerDataQueryBundle.php
+++ src/MaltehuebnerDataQueryBundle.php
@@ -2,9 +2,23 @@
 
 namespace MalteHuebner\DataQueryBundle;
 
+use Maltehuebner\DataQueryBundle\DependencyInjection\Compiler\ParameterPass;
+use Maltehuebner\DataQueryBundle\DependencyInjection\Compiler\QueryPass;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class MaltehuebnerDataQueryBundle extends Bundle
 {
+    /**
+     * {@inheritdoc}
+     *
+     * @return void
+     */
+    public function build(ContainerBuilder $container)
+    {
+        parent::build($container);
 
+        $container->addCompilerPass(new ParameterPass());
+        $container->addCompilerPass(new QueryPass());
+    }
 }
