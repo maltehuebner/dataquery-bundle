d4af0994df8b942d286780222a91a701d22f8073
Malte Hübner <maltehuebner@gmx.org>
2022-10-23 16:38:17 +0200

    Add configuration.

---
+++ src/DependencyInjection/Configuration.php
@@ -0,0 +1,13 @@
+<?php declare(strict_types=1);
+
+namespace MalteHuebner\DataQueryBundle\DependencyInjection;
+
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+
+class Configuration implements ConfigurationInterface
+{
+    public function getConfigTreeBuilder()
+    {
+        // TODO: Implement getConfigTreeBuilder() method.
+    }
+}
---
+++ src/DependencyInjection/MalteHuebnerDataQueryExtension.php
@@ -0,0 +1,21 @@
+<?php declare(strict_types=1);
+
+namespace MalteHuebner\DataQueryBundle\DependencyInjection;
+
+use Symfony\Bundle\FrameworkBundle\DependencyInjection\Configuration;
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader;
+use Symfony\Component\DependencyInjection\Extension\Extension;
+
+class MalteHuebnerDataQueryExtension extends Extension
+{
+    public function load(array $configs, ContainerBuilder $container): void
+    {
+        $configuration = new Configuration(false);
+        $config = $this->processConfiguration($configuration, $configs);
+
+        $xmlLoader = new Loader\XmlFileLoader($container, new FileLocator(__DIR__ . '/../../config'));
+        $xmlLoader->load('services.xml');
+    }
+}
