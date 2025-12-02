ef67ab16950d9dc81e86e8ad231f9675e1e2506c
Malte Hübner <maltehuebner@gmx.org>
2025-12-02 13:22:43 +0100

    Fix config.

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -2,13 +2,19 @@
 
 namespace MalteHuebner\DataQueryBundle\DependencyInjection;
 
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
 
 class Configuration implements ConfigurationInterface
 {
     #[\Override]
-    public function getConfigTreeBuilder()
+    public function getConfigTreeBuilder(): TreeBuilder
     {
-        // TODO: Implement getConfigTreeBuilder() method.
+        // Alias des Bundles (siehe getAlias() in deiner Extension!)
+        $treeBuilder = new TreeBuilder('malte_huebner_data_query');
+
+        $rootNode = $treeBuilder->getRootNode();
+
+        return $treeBuilder;
     }
 }
